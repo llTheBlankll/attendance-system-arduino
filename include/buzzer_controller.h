91ff6ce24cdaa27bf7a940a0bc72f1d1ae31d511
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-01-20 19:17:53 +0800

    Optimize buzzer.

--- include/buzzer_controller.h
+++ include/buzzer_controller.h
@@ -4,19 +4,12 @@
 
 #ifndef BUZZER_CONTROLLER_H
 #define BUZZER_CONTROLLER_H
-#define BUZZER_PIN 15
-#include <neotimer.h>
 
 class BuzzerController {
 public:
     BuzzerController();
 
-    void buzzTimerPoll();
-
     void buzz(int time);
-
-private:
-    Neotimer buzzTimer;
 };
 
 #endif //BUZZER_CONTROLLER_H
--- src/functions/buzzer_controller.cpp
+++ src/functions/buzzer_controller.cpp
@@ -2,29 +2,12 @@
 // Created by neltri on 1/20/24.
 //
 
+#include <Arduino.h>
 #include <buzzer_controller.h>
-
-Neotimer buzzTimer(1000);
-
-void BuzzerController::buzzTimerPoll() {
-    if (buzzTimer.done()) {
-        noTone(BUZZER_PIN);
-        buzzTimer.reset();
-    }
-}
+#define BUZZER_PIN 15
 
 void BuzzerController::buzz(const int time) {
-    buzzTimer.set(time);
-    buzzTimer.start();
-
-    if (buzzTimer.done()) {
-        noTone(BUZZER_PIN);
-    }
-
-    if (buzzTimer.waiting()) {
-        tone(BUZZER_PIN, 1000);
-    }
+  tone(BUZZER_PIN, 1000, time);
 }
 
-BuzzerController::BuzzerController() : buzzTimer(1000) {
-}
+BuzzerController::BuzzerController() = default;
